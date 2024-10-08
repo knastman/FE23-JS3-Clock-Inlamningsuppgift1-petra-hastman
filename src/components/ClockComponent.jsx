c473522c063f89cda8167099027be01790b65bc0
Petra <144622676+knastman@users.noreply.github.com>
2024-10-09 00:54:08 +0200

    Fix for ' vs "

--- src/components/ClockComponent.jsx
+++ src/components/ClockComponent.jsx
@@ -4,7 +4,7 @@ export default function DisplayClock( props ) {
   return (
     <div>
       <h2 className="clockHeader">Current Time</h2>
-      <p className='clockTime'>{props.time}</p>
+      <p className="clockTime">{props.time}</p>
     </div>
   );
 }
